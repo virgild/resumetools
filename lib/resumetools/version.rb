c62dbbbc68090dd3003ed2e264e1a0089d2e2378
Virgil Dimaguila <virgil@roundysoft.com>
2009-08-11 16:53:49 -0400

    Ticked minor gem version

--- lib/resumetools/version.rb
+++ lib/resumetools/version.rb
@@ -28,7 +28,7 @@ unless defined? ResumeTools::VERSION
     module VERSION
       MAJOR = 0
       MINOR = 2
-      TINY  = 0
+      TINY  = 1
       STRING = [MAJOR, MINOR, TINY].join('.')
     end
   end
